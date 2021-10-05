82d68208d2e458ab8a28721dfc6bcac334586635
Reginaldo Rubens da Silva <reginaldorubens@gmail.com>
2021-10-05 15:14:43 -0300

    Code optimization
    Optimization for eliminate duplicated code.

--- index.php
+++ index.php
@@ -24,21 +24,15 @@ $client->on('connect.after.each', function ($connection, $write) {
     $write->ircPrivmsg($canal, 'Gringinho Bot is ON | Gringinho Bot tá ON');
 });
  
-function traduzirPt($word) {
+function traduzir($word, $lang) {
+    $params = array( 
+        'pt' => array('source' => 'pt', 'target' => 'en'),
+        'en' => array('source' => 'en', 'target' => 'pt')
+    );
     $tr = new GoogleTranslate();
-    $tr->setSource('pt'); 
+    $tr->setSource($params[$lang]['source']); 
     $tr->setSource(); 
-    $tr->setTarget('en');
-
-    return $tr->translate($word);
-}
-
-function traduzirEn($word) {
-        
-    $tr = new GoogleTranslate();
-    $tr->setSource('en'); 
-    $tr->setSource(); 
-    $tr->setTarget('pt');
+    $tr->setTarget($params[$lang]['target']);
 
     return $tr->translate($word);
 }
@@ -49,19 +43,12 @@ $client->on('irc.received', function ($message, $write, $connection, $logger) {
     $command = explode(' ', $message['params']['text']);
 
     if ($message['command'] == 'PRIVMSG') {
-        if($command[0] == '!traduzir') {
-            $word = str_replace('!traduzir', ' ', $message['params']['text']);
-            $traducao = traduzirPt($word);
-            $write->ircPrivmsg($canal, $traducao);
-        }
-        
-        if($command[0] == '!translate') {
-            $word = str_replace('!translate', ' ', $message['params']['text']);
-            $traducao = traduzirEn($word);
+        if($command[0] == '!traduzir' || $command[0] == '!translate') {
+            $word = str_replace($command[0], ' ', $message['params']['text']);
+            $traducao = traduzir($word, $command[0] == '!traduzir' ? 'pt' : 'en');
             $write->ircPrivmsg($canal, $traducao);
         }
     }
-
 });
 
 $client->run($connection);
